41942e49d4edc6d43150b9a63338bdf8d91bb199
Andrii Dmytruk <admitruk237@gmail.com>
2024-03-24 16:07:23 +0100

    first commit

---
+++ 05-react-green-button/script.js
@@ -0,0 +1,8 @@
+const App = (
+  <div className="app">
+    <button onClick="">click me</button>
+  </div>
+);
+const container = document.getElementById("app");
+const root = ReactDOM.createRoot(container);
+root.render(App);
