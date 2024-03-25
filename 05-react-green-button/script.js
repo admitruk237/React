ff152b9272f223a054ef6bc729461e1579bec857
Andrii Dmytruk <admitruk237@gmail.com>
2024-03-25 15:08:52 +0100

    npmLive

--- 05-react-green-button/script.js
+++ 05-react-green-button/script.js
@@ -1,6 +1,8 @@
+const buttonText = "Click me please";
+
 const App = (
   <div className="app">
-    <button onClick="">click me</button>
+    <button onClick="">{buttonText}</button>
   </div>
 );
 const container = document.getElementById("app");
