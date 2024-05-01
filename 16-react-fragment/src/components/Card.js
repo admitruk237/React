ecb0c078d736e49276cd2a2c23c1dda4952d49be
Andrii Dmytruk <admitruk237@gmail.com>
2024-05-01 15:29:07 +0200

    React fragment

---
+++ 16-react-fragment/src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <React.Fragment>
+      <h1>Andrii</h1>
+      <h3>I am learning React course</h3>
+      <button>Like!</button>
+    </React.Fragment>
+  );
+};
+
+export default Card;
